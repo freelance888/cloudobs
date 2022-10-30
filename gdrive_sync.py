e4f0ec46d5a1f252d40b5272e06750c52b5d153b
Akmal Mukhsimov <aka.mukhsimov@gmail.com>
2022-10-30 18:36:38 +0500

    instance_service

--- gdrive_sync.py
+++ gdrive_sync.py
@@ -89,9 +89,9 @@ class DriveSync(threading.Thread):
 
                             if not self.files[fname]:
                                 try:
-                                    #gdown.download(id=fid, output=flocal, quiet=True)
                                     time.sleep(random.randint(3, 7))
-                                    gdown.download_via_gdrive_api(fid, flocal, api_key)
+                                    #gdown.download_via_gdrive_api(fid, flocal, api_key)
+                                    gdown.download(id=fid, output=flocal, quiet=True)
 
                                     if generate_file_md5(flocal) == fmd5Checksum:
                                         self.files[fname] = True
